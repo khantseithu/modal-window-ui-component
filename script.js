d4e54c3769e15c4f68f3cea0fa4e95ec5d92166c
khantseithu <khantseithuu@gmail.com>
2023-01-12 13:44:57 +0630

    finish up

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const showBtns = document.querySelectorAll('.show-modal');
+
+const modal = document.querySelector('.modal');
+
+const overlay = document.querySelector('.overlay');
+
+const closeBtn = document.querySelector('.close-modal');
+
+function addModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function removeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < showBtns.length; i++) {
+  showBtns[i].addEventListener('click', addModal);
+  closeBtn.addEventListener('click', removeModal);
+}
+
+document.addEventListener('keydown', function (evt) {
+  console.log(evt);
+
+  if (evt.key === 'Escape') {
+    removeModal();
+  }
+});
